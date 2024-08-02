3c6f2092cbba6df2485697045f2e984084f5cdb7
Mateo Arumugam <mateoarumugam@gmail.com>
2024-08-02 10:38:28 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Example JavaScript for form interaction
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Form submitted successfully!');
+});
